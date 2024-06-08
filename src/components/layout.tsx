0cce17ae932f6d71fa8455de3a0f77d91369c4f3
DasunK <dasunk1995@yahoo.com>
2024-06-08 13:11:30 +0530

    initial commit

---
+++ src/components/layout.tsx
@@ -0,0 +1,12 @@
+import Footer from './footer'
+import Navbar from './navbar'
+
+export default function Layout({ children }: any) {
+    return (
+        <>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
